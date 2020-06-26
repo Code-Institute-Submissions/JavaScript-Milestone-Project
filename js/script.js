093ac397ecd3ecd28e910f0c8bb8ff183039576e
Likibi <likibikangben@gmail.com>
2020-06-26 13:36:06 +0000

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
+          zoom: 6,
           center: {lat: 1.373333, lng: 32.290276}
         });
         var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        var locations = [
-            {lat: 0.347596, lng: 32.582520}
-        ];
+        var markers = locations.map(function(location, i) {
+            return new google.map.Marker({
+                position,
+                label: labels[i % labels.length]
+            });
+        });
 
         var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+        var locations = [
+            {lat: 0.347596, lng: 32.582520}
+        ];    
     
 };    
